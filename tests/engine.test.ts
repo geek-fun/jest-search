b7e45eb7fcb0680f64f8dd708088de56a246d8fd
seven <zilisheng1996@gmail.com>
2023-07-09 16:49:18 +0800

    integrate lint and test coverage report

--- tests/engine.test.ts
+++ tests/engine.test.ts
@@ -11,16 +11,13 @@ it('should start engine with default config', async () => {
 
   await stopEngine();
 
-  expect(JSON.parse(response.toString())).toEqual({
+  expect(JSON.parse(response.toString())).toMatchObject({
     name: 'jest-search-local',
     cluster_name: 'jest-search-local',
-    cluster_uuid: 'TVNghGWORHu0ae-RB-g41w',
     version: {
       number: '8.8.2',
       build_flavor: 'default',
       build_type: 'tar',
-      build_hash: '98e1271edf932a480e4262a471281f1ee295ce6b',
-      build_date: '2023-06-26T05:16:16.196344851Z',
       build_snapshot: false,
       lucene_version: '9.6.0',
       minimum_wire_compatibility_version: '7.17.0',
