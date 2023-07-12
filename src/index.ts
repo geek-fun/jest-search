1e98d2fa1aa97fd56ec62fc9a31d9a0fe1784b88
seven <zilisheng1996@gmail.com>
2023-07-12 22:15:08 +0800

    debug path issue

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import { startEngine, stopEngine, EngineType, EngineOptions } from './engine';
 import path from 'path';
+import { debug } from './debug';
 
 const globalSetup = async () => {
   const configPath =
     process.env.JEST_SEARCH_CONFIG || path.resolve(process.cwd() + '/jest-search-config.js');
-  const config = (await import(configPath))();
-  await startEngine(config);
+  debug(`configPath: ${configPath}, cwdPath: ${process.cwd()}`);
+  try {
+    const configFn = await import(configPath);
+    debug(`configFn: ${configFn}`);
+    const config = configFn();
+    debug(`final config: ${config}`);
+    await startEngine(config);
+  } catch (err) {
+    debug(`error caught: ${err}`);
+    throw err;
+  }
 };
 const globalTeardown = stopEngine;
 
